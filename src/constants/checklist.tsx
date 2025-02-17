dc03f4be041a70f10f0a87745f0074b00947d882
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2025-02-16 21:38:19 -0400

    v1.1: added extra checks + knockout stage

--- src/constants/checklist.tsx
+++ src/constants/checklist.tsx
@@ -2,24 +2,37 @@ export type Stage = {
     name: string;
     items: string[];
 }
+
+/**
+ * TODO:
+ *  - quais maltes vão só no final
+ */
+
 export const checklist: Stage[] = [{
     name: 'Dia anterior',
     items: [
+        'Água na panela',
         'Gelo para resfriamento',
         'AA dos lúpulos ajustado',
-        'Adição do floculante'
+        'Adição do floculante',
+        'Peso dos insumos verificado',
     ],
 }, {
     name: 'Setup',
     items: [
         'Filtro bazooca adicionado na panela',
-        'Jaqueta de neoprene instalada'
+        'Jaqueta de neoprene instalada',
+        'pHmetro calibrado',
+        'Densímetro calibrado',
     ]
 }, {
     name: 'Pré mash',
     items: [
         'Sais de correção adicionados na água',
         'Ácido ascórbico adicionado na água',
+        'Volume de água correto',
+        'Temperatura de mash correta verificada com receita',
+        'Temperatura de mash correta verificada com termômetro',
     ]
 }, {
     name: 'Mash',
@@ -31,6 +44,7 @@ export const checklist: Stage[] = [{
     items: [
         'Medição de pH pré fervura',
         'Medição de SG pré fervura',
+        'Estimativa de volume pré fervura',
         'Chiller imersão adicionado com 15min do fim da fervura',
         'Pá de whirlpool adicionada com 15min do fim da fervura',
         'Floculante adicionado com 10min do fim da fervura',
@@ -49,6 +63,11 @@ export const checklist: Stage[] = [{
         'Mangueira de saída de mosto sanitizada',
         'Amostra 200ml para medições finais'
     ]
+}, {
+    name: 'Knock-out',
+    items: [
+        'Medição de volume para fermentação'
+    ],
 }, {
     name: 'Inoculação',
     items: [
